6aaeaa88548a05eb5427934b8e51afc64107b64a
Vincent Brodin <64703732+kong-code@users.noreply.github.com>
2022-05-15 16:43:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
-const port = 5000;
+const port = process.env.PORT || 5000;
 let movies = [
     {
         name: 'MoonKnight',
